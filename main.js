8e36439a4aa9aed5b61a9b5f6ae2aba216c805d1
Gildas Philippe <gphilippe@deezer.com>
2020-06-14 19:04:50 +0200

    Load songs + category management

--- main.js
+++ main.js
@@ -1,43 +1,145 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
+const fs = require('fs');
+const lineByLine = require('n-readlines');
+const mm = require('music-metadata');
+const ID3Writer = require('browser-id3-writer');
 
+const audioFormats = [".wav", ".mp3"];
+const categoriesPath = "static/categories.txt";
+const categories = readCategories();
+
+
+// APP SETINGS
+
+let mainWindow;
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
     }
   })
-
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+  sendCategories();
 })
-
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+// LOAD DATA
+
+ipcMain.on('source:updateFolder', function(e, fold){
+  songs = getAllSongs(fold);
+  if(songs.length>0) getMetadata(songs);
+});
+
+function getAllSongs(folder){
+  let allFiles = [];
+  let files = fs.readdirSync(folder);
+  files.forEach(function(fname){
+    fullFname = path.join(folder, fname);
+    if(fs.lstatSync(fullFname).isDirectory()){
+      allFiles = allFiles.concat(getAllSongs(fullFname));
+    }
+    else{
+      if(audioFormats.includes(fname.substr(fname.length-4, fname.length))){
+        allFiles.push({
+          "fname": fname,
+          "path": fullFname
+        });
+      }
+    }
+  });
+  return allFiles;
+}
+
+async function getMetadata(songs){
+  let songsData = [];
+  for (const song of songs){
+    let fname = song["path"];
+    const metadata = await mm.parseFile(fname);
+    song["artist"] = metadata.common.artist;
+    song["title"] = metadata.common.title;
+    song["picture"] = metadata.common.picture;
+    if(song["artist"]==undefined){
+      song["artist"] = "";
+    } if(song["title"]==undefined){
+      song["title"] = "";
+    }
+    songsData.push(song);
+  }
+  mainWindow.webContents.send('source:load', songsData);
+}
+
+
+// WRITE DATA
+
+function writeMetadata(){
+  let fname_in = 'static/songs/Cee-Roo\ -\ Moses.mp3';
+  let img_name_in = 'static/img/u2.jpg';
+  let fname_out = 'static/songs/Me - My\ song.mp3'
+  
+  const songBuffer = fs.readFileSync(fname_in);
+  const coverBuffer = fs.readFileSync(img_name_in);
+  const writer = new ID3Writer(songBuffer);
+  writer.setFrame('TIT2', 'My new song')
+        .setFrame('TPE1', ['Me'])
+        .setFrame('TALB', 'EP88')
+        .setFrame('TYER', 2017)
+        /*.setFrame('APIC', {
+            type: 3,
+            description: 'Super picture'
+        })*/;
+  writer.addTag();
+  
+  const taggedSongBuffer = Buffer.from(writer.arrayBuffer);
+  fs.writeFileSync(fname_out, taggedSongBuffer);
+}
+
+
+// CATEGORIES MANAGEMENT
+
+ipcMain.on('category:get', function(e, category){
+  mainWindow.webContents.send('category:get', categories);
+});
+
+ipcMain.on('category:add', function(e, category){
+  if(!categories.includes(category)){
+    categories.push(category);
+    let data = categories.join("\n")+"\n";
+    fs.writeFile(categoriesPath, data, callback=function(){});
+    sendUpdatedCategories();
+  }
+});
+
+ipcMain.on('category:remove', function(e, category){
+  if(categories.includes(category)){
+    const index = categories.indexOf(category);
+    if (index > -1) categories.splice(index, 1);
+    let data = categories.join("\n")+"\n";
+    fs.writeFile(categoriesPath, data, callback=function(){});
+    sendUpdatedCategories();
+  }
+});
+
+function sendUpdatedCategories(){
+  mainWindow.webContents.send('category:update', categories);
+}
+
+function readCategories(){
+  const liner = new lineByLine(categoriesPath);
+  let categories = [];
+  let line;
+  while (line = liner.next()) categories.push(line.toString('utf8'));
+  return categories;
+}
--- renderer.js
+++ renderer.js
@@ -1,6 +1,111 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+
+const ipc = require('electron').ipcRenderer;
+const tbody = document.querySelector("#songList tbody");
+const categoriesUl = document.querySelector("#categoriesModalContent ul");
+let categoriesList;
+let fold = 'static/songs';
+
+// SETTING UP
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, "options");
+});
+window.onload = function(){
+  ipc.send('category:get', "");
+}
+
+// LOAD DATA
+
+load.addEventListener('click', function(){
+  sourceInput.click()
+})
+sourceInput.addEventListener('change', function(e, sour){
+  let folder = getFolder(sourceInput);
+  if(folder.length > 0) ipc.send('source:updateFolder', folder);
+});
+ipc.on('source:load', function (event) {
+  tbody.innerHTML = "";
+  data.forEach(song => {
+    addRow(song)
+  });
+})
+
+function getFolder(fileInput){
+  let path = "";
+  if(fileInput.files.length > 0){
+    let file = fileInput.files[0]
+    path = file.path.substr(0, file.path.length - file.name.length);
+  }
+  return path;
+}
+
+function addRow(song){
+  let row = `
+    <td class="iconCell"></td>
+    <td>${song["fname"]}</td>
+    <td>${song["artist"]}</td>
+    <td>${song["title"]}</td>
+    <td></td>
+    <td><label><input type="checkbox" class="filled-in" checked="checked"/><span></span></label></td>
+  `;
+  const tr = document.createElement('tr');
+  tr.innerHTML = row;
+  if(song["picture"] != undefined){
+    const cover = document.createElement('img');
+    let picture = song["picture"][0];
+    cover.setAttribute("src", `data:${picture.format};base64,${btoa(Uint8ToString(picture.data))}`);
+    cover.setAttribute("class", "cover responsive-img");
+    tr.querySelector(".iconCell").appendChild(cover)
+  }
+  tbody.appendChild(tr);
+}
+
+function Uint8ToString(u8a){
+  var CHUNK_SZ = 0x8000;
+  var c = [];
+  for (var i=0; i < u8a.length; i+=CHUNK_SZ) {
+    c.push(String.fromCharCode.apply(null, u8a.subarray(i, i+CHUNK_SZ)));
+  }
+  return c.join("");
+}
+
+// WRITE DATE
+
+destination.addEventListener('click', function(){
+  destinationInput.click()
+})
+
+// CATEGORIES MANAGEMENT
+
+ipc.on('category:get', function(event, categories){
+  categoriesList = categories;
+  categories.forEach(category => displayCategory(category))
+})
+ipc.on('category:update', function(event, categories){
+  categoriesList = categories;
+})
+
+newCategoryForm.addEventListener("submit", function(event){
+  event.preventDefault();
+  let category = newCategoryInput.value;
+  newCategoryInput.value = "";
+  displayCategory(category);
+  ipc.send('category:add', category);
+})
+
+function displayCategory(category){
+  const li = document.createElement('li');
+  li.classList.add('collection-item');
+  li.innerHTML = `<div>${category}<a href="#!" class="secondary-content remove-category">Remove</a></div>`;
+  li.querySelector('.remove-category').addEventListener("click", function(){
+    removeCategory(category, li);
+  })
+  categoriesUl.appendChild(li);
+}
+
+function removeCategory(category, li){
+  li.remove();
+  ipc.send('category:remove', category);
+}
+
